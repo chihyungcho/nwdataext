44d82a5166b7e983b31200859f85bce6d1f05637
CH <realfine_kr@hotmail.com>
2019-05-28 15:32:12 +0900

    dataext.py update

--- dataext.py
+++ dataext.py
@@ -1,12 +1,17 @@
 from jinja2 import Environment, FileSystemLoader
 import yaml
-with open("int.yaml") as int_yaml:
-    interface_yaml = yaml.load(int_yaml)
+
+def open_yaml():
+    with open("int.yaml") as int_yaml:
+        interface_yaml = yaml.load(int_yaml)
 
 
+def render_yaml():
+    ENV = Environment(loader=FileSystemLoader('.'))
+    template = ENV.get_template("core.j2")
+    print(template.render(interfaces=interface_yaml))
 
-ENV = Environment(loader=FileSystemLoader('.'))
-template = ENV.get_template("core.j2")
-
-
-print(template.render(interfaces=interface_yaml))
+def main():
+    
+if __name__ == "__main__":
+    main()
