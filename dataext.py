6798d887df7d2f803f34882cfa28ea6e1ac6a65e
Chihyung Cho <chihyung.cho@gmail.com>
2019-06-27 07:53:40 +0900

    added 'save_in_yaml' function

--- dataext.py
+++ dataext.py
@@ -27,8 +27,6 @@ def get_connection(sship, sshid, sshpw):
 # Parsing cisco config using CiscoConfParse library and tranform the data into YAML format.
 # Currently transitioning from a method to a class.
 class BGP_Extraction:
-    # things in () are for inheritance
-
 #   Parsing cisco config using CiscoConfParse library.
     def __init__(self, confdata):
         self.confdata = confdata
@@ -41,7 +39,7 @@ class BGP_Extraction:
         return bgp_asnum
 
 # Extracting bgp router ID
-    def rtrid(self):
+    def rtr_id(self):
         bgp_rtrids = self.bgp_cmds[0].re_search_children\
                     (r'^ bgp router-id [1-2]?[0-9]?[0-9](\.[1-2]?[0-9]?[0-9]){3}$')
         bgp_rtrid = bgp_rtrids[0].text[len(' bgp router-id '):]
@@ -70,21 +68,18 @@ class BGP_Extraction:
             j += 1
             network = net.text[1:].split()
             net_dict["network"+str(j)] = [network[1], network[3]]
-        # print('net_dict: ', net_dict)
         return net_dict
 
 # Extracting BGP maximum-paths
     def maxpath(self):
         bgp_maxpaths = self.bgp_cmds[0].re_search_children(r'^ maximum-paths \d$')
         bgp_maxpath = bgp_maxpaths[0].text[len(' maximum-paths '):]
-        # print('bgp_maxpath: ', bgp_maxpath)
         return bgp_maxpath
 
-def save_in_yaml(**parsed_data):
-    #   saving config in a YAML file
-    # with open('existing_router1.yaml', 'w') as ext_yaml:
-    #     yaml.dump()
-    pass
+def save_in_yaml(parsed_data, rtr_name):
+# saving config in a YAML file
+    with open('existing_'+rtr_name+'_conf.yaml', 'w') as ext_yaml:
+        yaml.dump(parsed_data, ext_yaml, default_flow_style=False)
 
 def precheck():
     #   precheck function
@@ -107,42 +102,44 @@ def restore_traffic():
     pass
 
 def main():
-# Pulling the management connection info from a YAML.
-    mgmt = open_rtr_mgmt_yaml()
-    print('Loading router mgmt info: {}'. format(mgmt))
-    ip1 = mgmt[0]['mgmt_ip']
-    id1 = mgmt[0]['id']
-    pw1 = mgmt[0]['pw']
-    ip2 = mgmt[1]['mgmt_ip']
-    id2 = mgmt[1]['id']
-    pw2 = mgmt[1]['pw']
+# # Pulling the management connection info from a YAML.
+#     mgmt = open_rtr_mgmt_yaml()
+#     print('Loading router mgmt info: {}'. format(mgmt))
+#     ip1 = mgmt[0]['mgmt_ip']
+#     id1 = mgmt[0]['id']
+#     pw1 = mgmt[0]['pw']
+#     ip2 = mgmt[1]['mgmt_ip']
+#     id2 = mgmt[1]['id']
+#     pw2 = mgmt[1]['pw']
 
-# Connecting to the routers.
-    print('Connecting to Router1 IP: {0}'.format(ip1))
-    net_connect1 = get_connection(ip1, id1, pw1)
-    print('Connecting to Router2 IP: {0}'.format(ip2))
-    net_connect2 = get_connection(ip2, id2, pw2)
-    # print("Router1 Prompt: ", net_connect1.find_prompt())
-    # print("Router2 Prompt: ", net_connect2.find_prompt())
+# # Connecting to the routers.
+#     print('Connecting to Router1 IP: {0}'.format(ip1))
+#     net_connect1 = get_connection(ip1, id1, pw1)
+#     print('Connecting to Router2 IP: {0}'.format(ip2))
+#     net_connect2 = get_connection(ip2, id2, pw2)
+#     # print("Router1 Prompt: ", net_connect1.find_prompt())
+#     # print("Router2 Prompt: ", net_connect2.find_prompt())
 
-# Pulling the configs
-    net_connect1.send_command('terminal length 0')
-    net_connect2.send_command('terminal length 0')
-    output1 = net_connect1.send_command('show run')
-    output2 = net_connect2.send_command('show run')
-    print(output1)
-    print('Saving the router1 config...')
-    with open('router1_bkup.conf', 'w') as router1_bkup:
-        router1_bkup.write(output1)
-    print(output2)
-    print('Saving the router2 config...')
-    with open('router2_bkup.conf', 'w') as router2_bkup:
-        router2_bkup.write(output2)
+# # Pulling the configs
+#     net_connect1.send_command('terminal length 0')
+#     net_connect2.send_command('terminal length 0')
+#     output1 = net_connect1.send_command('show run')
+#     output2 = net_connect2.send_command('show run')
+#     print(output1)
+#     print('Saving the router1 config...')
+#     with open('router1_bkup.conf', 'w') as router1_bkup:
+#         router1_bkup.write(output1)
+#     print(output2)
+#     print('Saving the router2 config...')
+#     with open('router2_bkup.conf', 'w') as router2_bkup:
+#         router2_bkup.write(output2)
 
 # Parsing the config
     rtr1 = BGP_Extraction("router1_bkup.conf")
-    rtr2 = BGP_Extraction("router2_bkup.conf")
-
+    # rtr2 = BGP_Extraction("router2_bkup.conf")
+    bgp_conf1 = {'bgp_asnum': rtr1.as_num(), 'bgp_rtrid': rtr1.rtr_id(), 'bgp_nei': rtr1.nei(), 'bgp_net': rtr1.net(), 'bgp_maxpath': rtr1.maxpath()}
+    # print(bgp_conf1)
+    save_in_yaml(bgp_conf1, 'router1')
 
 if __name__ == "__main__":
     main()
